3566b3664bdc54596b64a90c469839b7dec2a289
AnanthiMuthu02 <ananthi021298@gmail.com>
2024-07-23 23:29:41 +0500

    still working on cors no luck till now

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 
 // CORS configuration
-const allowedOrigins = ['http://localhost:3000', 'https://recepie-web-app.vercel.app'., 'https://recepie-web-app.vercel.app/' ];
+const allowedOrigins = ['https://recepie-web-app.vercel.app', 'https://recepie-web-app.vercel.app/', 'http://localhost:3000', ];
 
 app.use(cors({
   credentials: true,
