2c53b50f9e378b46be4514b06963674de364465b
AnanthiMuthu02 <ananthi021298@gmail.com>
2024-07-23 20:57:50 +0500

    removed node modules from git

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -1,10 +1,11 @@
 const Router = require('express')
 const {createRecipe, getRecipes, getRecipe} = require('../controllers/recipeController')
 const authMiddleware = require('../middleware/authMiddleware')
+const fileUpload = require('../middleware/fileUpload'); 
 
 const router = Router()
 
-router.post('/',authMiddleware, createRecipe)  
+router.post('/', authMiddleware, fileUpload.single('thumbnail'), createRecipe);
 router.get('/', getRecipes)
 router.get('/:id', getRecipe)
 
